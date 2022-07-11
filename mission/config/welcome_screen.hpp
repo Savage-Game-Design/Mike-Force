286068697c1daee42764e5569ae9fe6e55222071
Spoffy <contact@spoffy.net>
2022-07-11 05:19:32 +0100

    Removes "Beta" from description.ext and updates welcome screen

--- mission/config/welcome_screen.hpp
+++ mission/config/welcome_screen.hpp
@@ -5,12 +5,12 @@ class ParaWelcomeScreen {
         title = "Welcome to Mike Force";
         /*content = "Structured Text Content";*/
 		content[] = {
-			"<t align='center' size='1.5'>Welcome to the Mike Force beta!</t>",
+			"<t align='center' size='1.5'>Welcome to Mike Force!</t>",
 			"<br/><br/>",
 			"<t align='center'>",
-			"This mission is still under heavy development, so please keep an eye on the <a href='https://steamcommunity.com/sharedfiles/filedetails/?id=2477873447'>Steam Workshop</a> page for updates!",
+			"This mission is constantly under development, so please keep an eye on the <a href='https://steamcommunity.com/sharedfiles/filedetails/?id=2477873447'>Steam Workshop</a> page for updates!",
 			"<br/><br/>",
-			"If you're new to the gamemode, you can find guidance in the field manual (Esc -> Field Manual), with a full tutorial coming soon",
+			"If you're new to the gamemode, you can find guidance in the field manual (Esc -> Field Manual), although you'll get helpful prompts as you play.",
 			"<br/><br/>",
 			"You can report issues on <a href='https://github.com/Savage-Game-Design/Mike-Force'>Github</a>, which is linked from the workshop.",
 			"<br/><br/>",
