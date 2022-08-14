cfb5733f3ca4f5b8241980585631b35ec260d905
Rasmus Tanggaard <rasmus@tthost.dk>
2022-08-14 21:43:22 +0200

    Fix user_paths_example.py to match requested

---
+++ user_paths_example.py
@@ -0,0 +1,4 @@
+# Copy and paste your address bar contents for each of your desired locations here. 
+# Remove "_example" from the name of this file.
+MISSIONS_PATH = r"E:\Documents\Arma 3\MPMissions"
+PARADIGM_PATH = r"E:\Documents\Arma 3\SGD_Paradigm"
